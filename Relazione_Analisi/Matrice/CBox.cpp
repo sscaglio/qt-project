07ea66751f47839dfb6df5edb7df75aaf048d935
sscaglio <sscaglio@t34.torre.math.unipd.it>
2018-01-16 14:14:51 +0100

    aggiornato comportamento matrice B|S|C Box

---
+++ Relazione_Analisi/Matrice/CBox.cpp
@@ -0,0 +1,102 @@
+//MatriceConcreta<T>
+
+
+mambri interni
+
+campi dati
+
+QVector<T> matrice
+const unsigned int righe
+const unsigned int colonne
+const Boundchecker<T> bd
+
+
+Interfaccia
+
+metodi
+
+Matrice(unsigned int r,unsigned int c):
+       matrice(QVector(righe * colonne)),righe(r),colonne(c){
+}
+
+Matrice*
+operator+(const Matrice& rht)const{
+  try{
+    if(!((righe == rht.righe) ||
+	 (colonne != rht.colonne))){// dimensioni non consistenti
+      throw domain_error("dimensioni matrice non consistenti");
+    }
+    Matrice<T> *res = new Matrice<T>(righe * colonne);
+    for(int indiceElt = 0;
+	indiceElt < matrice.size();
+	++indiceElt){
+      int sommaElemento = 0;
+      if(bd.addConsistent(matrice[i],rht.matrice[i])){
+	sommaElemento = matrice[i] + rht.matrice[i];
+      }
+      res[i] = sommaElemento;
+    }
+    return res;
+  }catch(exception & e){
+    res.clear();// elimina ogni elemento appartente a res
+    delete res;// dealloca puntatore
+    e.what();// stampa messaggio errore
+  }
+}
+
+
+Matrice*
+operator-(const Matrice& rht)const{
+  try{
+    if(!((righe == rht.righe) ||
+	 (colonne != rht.colonne))){// dimensioni non consistenti
+      throw domain_error("dimensioni matrice non consistenti");
+    }
+    Matrice<T> *res = new Matrice<T>(righe * colonne);
+    for(int indiceElt = 0;
+	indiceElt < matrice.size();
+	++indiceElt){
+      int differenzaElemento = 0;
+      if(bd.subConsistent(matrice[i],rht.matrice[i])){
+	differenzaElemento = matrice[i] - rht.matrice[i];
+      }
+      res[i] = differenzaElemento;
+    }
+    return res;
+  }catch(exception & e){
+    res.clear();// elimina ogni elemento appartente a res
+    delete res;// dealloca puntatore
+    e.what();// stampa messaggio errore
+  }
+}
+
+
+
+Matrice*
+operator*(const Matrice& rht)const{
+  try{
+    if(colonne != rht.righe)
+      throw domain_error("dimensioni matrice non consistenti");
+    }
+    Matrice<T> *res = new Matrice<T>(righe * colonne);
+    for(int i = 0 ; i < righe;++i){
+      for(int j = 0; j < rht.colonne;++j){
+	T sommaParziale = T();
+	for(int k = 0; k < colonne;++i){
+	  if(bd.addConsistent
+	     (matrice[i * colonna + k],rht.matrice[colonna * k + j]))
+	    sommaParziale =
+	      matrice[i * colonna + k] + rht.matrice[colonna * k + j]
+	}
+	res.push_back(sommaParziale);
+      }
+    }
+    
+    return res;
+  }catch(exception & e){
+    res.clear();// elimina ogni elemento appartente a res
+    delete res;// dealloca puntatore
+    e.what();// stampa messaggio errore
+  }
+}
+
